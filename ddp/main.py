31648ebf29adda12ba52871c171c6b570d772aca
Ricky Gal <ricky.gal@ds2.com>
2023-07-09 18:01:10 -0500

    Added model

--- ddp/main.py
+++ ddp/main.py
@@ -1,13 +1,35 @@
 import os
 import tempfile
 import torch
+from absl import flags
 import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn as nn
 import torch.optim as optim
-
 from torch.nn.parallel import DistributedDataParallel as DDP
 
+import model as model_definition
+
+FLAGS = flags.FLAGS
+flags.DEFINE_float("lr", default=0.01, help="Learning rate value.")
+flags.DEFINE_integer("seed", default=1234, help="Seed value.")
+flags.DEFINE_integer("batch_size", default=128, help="Maximum batch size.")
+flags.DEFINE_integer("validation_batch_size", default=64, help="Maximum batch size during model validation.")
+flags.DEFINE_integer("vector_length", default=50, help="Length of entity/relation vector.")
+flags.DEFINE_float("margin", default=1.0, help="Margin value in margin-based ranking loss.")
+flags.DEFINE_integer("norm", default=1, help="Norm used for calculating dissimilarity metric (usually 1 or 2).")
+flags.DEFINE_integer("epochs", default=2000, help="Number of training epochs.")
+flags.DEFINE_string("dataset_path", default="./synth_data", help="Path to dataset.")
+flags.DEFINE_bool("use_gpu", default=True, help="Flag enabling gpu usage.")
+flags.DEFINE_integer("validation_freq", default=10, help="Validate model every X epochs.")
+flags.DEFINE_string("checkpoint_path", default="", help="Path to model checkpoint (by default train from scratch).")
+flags.DEFINE_string("tensorboard_log_dir", default="./runs", help="Path for tensorboard log directory.")
+
+HITS_AT_1_SCORE = float
+HITS_AT_3_SCORE = float
+HITS_AT_10_SCORE = float
+MRR_SCORE = float
+METRICS = Tuple[HITS_AT_1_SCORE, HITS_AT_3_SCORE, HITS_AT_10_SCORE, MRR_SCORE]
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
@@ -63,7 +85,34 @@ def demo_checkpoint(rank, world_size):
     print(f"Running DDP checkpoint example on rank {rank}.")
     setup(rank, world_size)
 
-    model = ToyModel().to(rank)
+    torch.random.manual_seed(FLAGS.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    path = FLAGS.dataset_path
+    train_path = os.path.join(path, "train.txt")
+    validation_path = os.path.join(path, "valid.txt")
+    test_path = os.path.join(path, "test.txt")
+
+    entity2id, relation2id = data.create_mappings(train_path)
+
+    batch_size = FLAGS.batch_size
+    vector_length = FLAGS.vector_length
+    margin = FLAGS.margin
+    norm = FLAGS.norm
+    learning_rate = FLAGS.lr
+    epochs = FLAGS.epochs
+    device = torch.device('cuda') if FLAGS.use_gpu else torch.device('cpu')
+
+    train_set = data.FB15KDataset(train_path, entity2id, relation2id)
+    train_generator = torch_data.DataLoader(train_set, batch_size=batch_size)
+    validation_set = data.FB15KDataset(validation_path, entity2id, relation2id)
+    validation_generator = torch_data.DataLoader(validation_set, batch_size=FLAGS.validation_batch_size)
+    test_set = data.FB15KDataset(test_path, entity2id, relation2id)
+    test_generator = torch_data.DataLoader(test_set, batch_size=FLAGS.validation_batch_size)
+    model = model_definition.TransE(entity_count=len(entity2id), relation_count=len(relation2id), dim=vector_length,
+                                    margin=margin,
+                                    device=device, norm=norm).to(rank)
     ddp_model = DDP(model, device_ids=[rank])
 
     loss_fn = nn.MSELoss()
@@ -90,7 +139,9 @@ def demo_checkpoint(rank, world_size):
     loss_fn = nn.MSELoss()
     loss_fn(outputs, labels).backward()
     optimizer.step()
-
+    scores = test(model=best_model, data_generator=test_generator, entities_count=len(entity2id), device=device,
+                  summary_writer=summary_writer, epoch_id=1, metric_suffix="test")
+    print("Test scores: ", scores)
     # Use a barrier() to make sure that all processes have finished reading the
     # checkpoint
     dist.barrier()
@@ -100,6 +151,53 @@ def demo_checkpoint(rank, world_size):
 
     cleanup()
 
+def test(model: torch.nn.Module, data_generator: torch_data.DataLoader, entities_count: int,
+         summary_writer: tensorboard.SummaryWriter, device: torch.device, epoch_id: int, metric_suffix: str,
+         ) -> METRICS:
+    examples_count = 0.0
+    hits_at_1 = 0.0
+    hits_at_3 = 0.0
+    hits_at_10 = 0.0
+    mrr = 0.0
+
+    entity_ids = torch.arange(end=entities_count, device=device).unsqueeze(0)
+    for head, relation, tail in data_generator:
+        current_batch_size = head.size()[0]
+
+        head, relation, tail = head.to(device), relation.to(device), tail.to(device)
+        all_entities = entity_ids.repeat(current_batch_size, 1)
+        heads = head.reshape(-1, 1).repeat(1, all_entities.size()[1])
+        relations = relation.reshape(-1, 1).repeat(1, all_entities.size()[1])
+        tails = tail.reshape(-1, 1).repeat(1, all_entities.size()[1])
+
+        # Check all possible tails
+        triplets = torch.stack((heads, relations, all_entities), dim=2).reshape(-1, 3)
+        tails_predictions = model.predict(triplets).reshape(current_batch_size, -1)
+        # Check all possible heads
+        triplets = torch.stack((all_entities, relations, tails), dim=2).reshape(-1, 3)
+        heads_predictions = model.predict(triplets).reshape(current_batch_size, -1)
+
+        # Concat predictions
+        predictions = torch.cat((tails_predictions, heads_predictions), dim=0)
+        ground_truth_entity_id = torch.cat((tail.reshape(-1, 1), head.reshape(-1, 1)))
+
+        hits_at_1 += metric.hit_at_k(predictions, ground_truth_entity_id, device=device, k=1)
+        hits_at_3 += metric.hit_at_k(predictions, ground_truth_entity_id, device=device, k=3)
+        hits_at_10 += metric.hit_at_k(predictions, ground_truth_entity_id, device=device, k=10)
+        mrr += metric.mrr(predictions, ground_truth_entity_id)
+
+        examples_count += predictions.size()[0]
+
+    hits_at_1_score = hits_at_1 / examples_count * 100
+    hits_at_3_score = hits_at_3 / examples_count * 100
+    hits_at_10_score = hits_at_10 / examples_count * 100
+    mrr_score = mrr / examples_count * 100
+    summary_writer.add_scalar('Metrics/Hits_1/' + metric_suffix, hits_at_1_score, global_step=epoch_id)
+    summary_writer.add_scalar('Metrics/Hits_3/' + metric_suffix, hits_at_3_score, global_step=epoch_id)
+    summary_writer.add_scalar('Metrics/Hits_10/' + metric_suffix, hits_at_10_score, global_step=epoch_id)
+    summary_writer.add_scalar('Metrics/MRR/' + metric_suffix, mrr_score, global_step=epoch_id)
+
+    return hits_at_1_score, hits_at_3_score, hits_at_10_score, mrr_score
 
 class ToyMpModel(nn.Module):
     def __init__(self, dev0, dev1):
